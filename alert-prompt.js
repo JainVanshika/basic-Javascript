fa1d6c62503fba03bcbed84275da62c5045b4251
Vanshika Jain <82704514+JainVanshika@users.noreply.github.com>
2023-08-05 16:54:47 +0530

    example of alert prompt func

---
+++ alert-prompt.js
@@ -0,0 +1,6 @@
+//example of alert and prompt function
+let fname = prompt("enter your first name:");
+let lname = prompt("enter your last name:");
+// console.log("welcome, ", fname, lname);
+let msg = `welcome, ${fname} ${lname}`;
+alert(msg);
