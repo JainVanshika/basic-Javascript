f9a7899619369a8029d1de49734adc77fb8134c2
Vanshika Jain <82704514+JainVanshika@users.noreply.github.com>
2023-08-22 22:36:34 +0530

    Update Object-Literals-part1.js

--- Object-Literals.js
+++ Object-Literals.js
@@ -1,4 +1,5 @@
 //object literals
+//basic syntax
 const student = {
   name: "vanshika",
   age: 21,
@@ -10,27 +11,3 @@ const item = {
   color: ["red", "pink"],
 };
 
-//twitter post
-//create an obj literal for the properties of twitter post which includes:
-//username,content,likes,reposts,Tags.
-const post = {
-  username: "@vanshika.jain31",
-  content: "this is my #first post",
-  likes: 150,
-  repost: 5,
-  tags: ["@apnacollege", "@gfg"],
-};
-console.log(post);
-console.log(post.username);
-console.log(post["username"]);
-
-//add update value
-post.likes = 555;
-post.username = "@vanshika";
-console.log(post);
-post.country = "india"; //update or add
-console.log(post);
-post.tags = "5M";
-console.log(post);
-delete post.country;
-console.log(post);
